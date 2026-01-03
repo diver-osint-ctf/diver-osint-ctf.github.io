6ef6f360c7a486564ad19f3a1517481cb904c306
ryo-a <ryoa.dev@gmail.com>
2026-01-03 23:53:48 +0900

    fix: missing config

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,9 @@ export default defineConfig({
                 main: resolve(__dirname, 'index.html'),
                 en: resolve(__dirname, 'en.html'),
                 vision: resolve(__dirname, 'vision.html'),
-                players_guide: resolve(__dirname, 'players_guide.html')
+                players_guide: resolve(__dirname, 'players_guide.html'),
+                rules_swimmer26: resolve(__dirname, 'rules_swimmer26.html'),
+                about_flags: resolve(__dirname, 'about_flags.html'),
             }
         }
     }
